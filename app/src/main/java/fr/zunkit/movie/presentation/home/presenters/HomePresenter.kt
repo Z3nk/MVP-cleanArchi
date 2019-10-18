0729cadf009fa799c30b8e39383e48f644a44061
z3nk <damien.gaillard86@gmail.com>
2019-10-18 13:59:33 +0200

    fix: tests

--- app/src/main/java/fr/zunkit/movie/presentation/home/presenters/HomePresenter.kt
+++ app/src/main/java/fr/zunkit/movie/presentation/home/presenters/HomePresenter.kt
@@ -2,24 +2,16 @@ package fr.zunkit.movie.presentation.home.presenters
 
 import fr.zunkit.movie.domain.movie.interactors.MovieInteractor
 import fr.zunkit.movie.presentation.model.Movie
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 
 
 class HomePresenter(val view: HomeViewListener, val interactor: MovieInteractor) {
-    fun getPopularMovies() {
+    suspend fun getPopularMovies() {
         try {
-            CoroutineScope(Dispatchers.IO).launch {
-                val response = interactor.getPopularMovies()
-                if (response.isSuccessful) {
-                    withContext(Dispatchers.Main) {
-                        response.body()?.results?.map { Movie(it) }?.let { view.displayMovies(it) }
-                    }
-                } else {
-                    view.displayErrorServer()
-                }
+            val response = interactor.getPopularMovies()
+            if (response.isSuccessful) {
+                response.body()?.results?.map { Movie(it) }?.let { view.displayMovies(it) }
+            } else {
+                view.displayErrorServer()
             }
         } catch (e: Exception) {
             view.displayErrorServer()
